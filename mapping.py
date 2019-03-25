aa37c5df659accc8ee909e093728f3d0250f49d2
Ben Edwards <ben@simplicat.com>
2019-03-25 14:07:39 +0000

    Print a fully formed URL

--- mapping.py
+++ mapping.py
@@ -1,6 +1,10 @@
 from polyline.codec import PolylineCodec
 import urllib.parse
-
+"""
+Turn a bunch of lat longs in to a usable mapbox url with an encoded polyline
+https://docs.mapbox.com/api/legacy/static-classic/
+This is so we can embed the map in a Wordpress blog.
+"""
 latlngs = [
     (28.92, -13.66),
     (27.3, -16.61),
@@ -15,11 +19,17 @@ latlngs = [
     (14.4, -45.02),
     (14.32, -47.2),
     (14.3, -50),
+    (14.48, -54.16),
+    (14.37, -56.25),
+    (14.25, -59.18)
     ]
 path=PolylineCodec().encode(latlngs)
-print(urllib.parse.quote(path))
-
+encoded = urllib.parse.quote(path)
+url = "https://api.mapbox.com/v4/mapbox.streets/path-3+f44-0.8+f44-0({})/auto/800x500.png?access_token=pk.eyJ1IjoiYmVuZWR3YXJkczEiLCJhIjoiY2pzeGRiemp3MDY4czQ0cW9sZHRlZHNtcCJ9.tHBKpUFYmXVEEjzPB5XRbw".format(encoded)
+print(url)
 """
+
+
 auto
 
 curl -g "https://api.mapbox.com/v4/mapbox.streets/path-3+f44-0.5+f44-0(_mooD%7E%7DjrA%7Ek%7BHnd_Qnz%7BJnaiI%7E%60%7DKnldJnhsKnldJ%7EpaF%7EvpQnkuC%7EggN%7EpdB%7Ezrc%40%7EcsS%7Ekl%5Cnm_A%7E%7EiZ%7EdtB%7EqpL%7ErN%7EwhL%7E%7BB%7EzaP)/auto/1000x500.png?access_token=pk.eyJ1IjoiYmVuZWR3YXJkczEiLCJhIjoiY2pzeGRiemp3MDY4czQ0cW9sZHRlZHNtcCJ9.tHBKpUFYmXVEEjzPB5XRbw"
